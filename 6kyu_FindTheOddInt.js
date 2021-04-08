14fe1ee0d5e817f9f7fb622580b94ece99e4243d
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-04-07 21:14:23 -0400

    Refactored 6kyu - Find The Odd Integer

--- 6kyu_FindTheOddInt.js
+++ 6kyu_FindTheOddInt.js
@@ -31,8 +31,3 @@ function findOdd(array) {
   }
   return parseInt(resultInt);
 }
-
-console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5])); // 5);
-console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5])); // -1);
-console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5])); // 5);
-console.log(findOdd([10])); // 10);
